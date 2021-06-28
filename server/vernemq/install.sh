b4a01c66aea7dd4525d192376d366799beb80f69
Daniel Lim <orcas@octalorca.me>
2021-06-28 08:49:04 +0800

    feat: vernemq systemd unit

--- server/vernemq/install.sh
+++ server/vernemq/install.sh
@@ -5,3 +5,4 @@
 
 ./generate_config.sh -c ../server.conf -o /usr/bin/vernemq/etc/vernemq.conf -u /usr/bin/vernemq/etc/vmq.passwd
 cp vmq.acl /usr/bin/vernemq/etc/vmq.acl
+cp ./run_vernemq.sh /usr/bin/vernemq/bin
