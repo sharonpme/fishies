cc0ee278d796e50d82a636404baefa85ba7754a6
Daniel Lim <orcas@octalorca.me>
2021-08-24 16:42:56 +0800

    fix: scheduler setup.sh copy pages to etc and remove systemd syslog integration

--- server/scheduler/setup.sh
+++ server/scheduler/setup.sh
@@ -6,6 +6,7 @@ mkdir -p /etc/fishies
 cp scheduler /etc/fishies/scheduler
 cp ../server.conf /etc/fishies/server.conf
 cp scheduler.conf /etc/fishies/scheduler.conf
+cp -r pages /etc/fishies/page
 
 cp scheduler.service /etc/systemd/system/fishies-scheduler.service
 
