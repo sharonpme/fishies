103a7540823b5220d5c4d27c614297d6f350bba4
Daniel Lim <orcas@octalorca.me>
2021-08-24 16:34:54 +0800

    feat: scheduler setup.sh setup systemd unit

--- server/scheduler/setup.sh
+++ server/scheduler/setup.sh
@@ -3,5 +3,11 @@ apt update && apt install -y golang-go
 go build
 
 mkdir -p /etc/fishies
-cp scheduler /etc/fishies
+cp scheduler /etc/fishies/scheduler
+cp ../server.conf /etc/fishies/server.conf
+cp scheduler.conf /etc/fishies/scheduler.conf
 
+cp scheduler.service /etc/systemd/system/fishies-scheduler.service
+
+systemctl daemon-reload
+systemctl restart fishies-scheduler.service
