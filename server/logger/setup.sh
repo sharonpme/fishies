dbb9faa5d29ae04d50a4edfdeee8d28da26becd9
Daniel Lim <orcas@octalorca.me>
2021-08-25 02:24:28 +0800

    fix: logger package lists

--- server/logger/setup.sh
+++ server/logger/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt update && apt install -y logstash filebeat
 
 cp filebeat.yml /usr/share/filebeat/filebeat.yml
