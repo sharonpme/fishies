0ba6c1c9fa1dedbaf4b921adb6e766f15712eabc
Daniel Lim <orcas@octalorca.me>
2021-08-24 23:43:59 +0800

    fix: renamed time timer unit

--- server/time/setup.sh
+++ server/time/setup.sh
@@ -1,10 +1,10 @@
 apt update && apt install -y mosquitto-clients date
 
 cp fishies-timer.service /etc/systemd/system/fishies-timer.service
-cp fishies-timer-timer.service /etc/systemd/system/fishies-timer-timer.service
+cp fishies-timer.timer /etc/systemd/system/fishies-timer.timer
 
 mkdir -p /etc/fishies
 cp ../server.conf /etc/fishies/server.conf
 
 systemctl daemon-reload
-systemctl restart fishies-timer-timer.service
+systemctl restart fishies-timer.timer
