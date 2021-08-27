89b5a7cf84f60d5f1bd845d2f4ece7650dc76b5d
Daniel Lim <orcas@octalorca.me>
2021-08-27 09:07:03 +0800

    fix: get logger working

--- server/logger/setup.sh
+++ server/logger/setup.sh
@@ -4,8 +4,11 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt update && apt install -y logstash filebeat
 
-cp filebeat.yml /usr/share/filebeat/filebeat.yml
-cp fishies.conf /usr/share/logstash/pipeline
+cp filebeat.yml /etc/filebeat/filebeat.yml
+cp fishies.conf /etc/logstash/conf.d/fishies.conf
+
+touch /etc/fishies/fishies.log
+chown logstash /etc/fishies/fishies.log
 
 systemctl restart logstash.service
 systemctl restart filebeat.service
