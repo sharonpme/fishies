fff50c05689fe033ad16938cb45211a0247d3d8a
Daniel Lim <orcas@octalorca.me>
2021-08-30 01:40:34 +0800

    feat: add mdns to hosts

--- server/mdns/setup.sh
+++ server/mdns/setup.sh
@@ -1,5 +1,6 @@
 apt update && apt install -y avahi-daemon
 cp mqtt.service /etc/avahi/services
+hostname -I | head -n 1 >> /etc/avahi/hosts
 
 systemctl enable avahi-daemon.service
 systemctl start avahi-daemon.service
