dbce81dbb6178f476af397160aad3e106b805e50
Daniel Lim <orcas@octalorca.me>
2021-08-30 02:03:11 +0800

    fix: forgot to add domain to mqtt hosts

--- server/mdns/setup.sh
+++ server/mdns/setup.sh
@@ -1,6 +1,6 @@
 apt update && apt install -y avahi-daemon
 cp mqtt.service /etc/avahi/services
-hostname -I | head -n 1 >> /etc/avahi/hosts
+hostname -I | head -n 1 | sed -e 's/$/mqtt/' >> /etc/avahi/hosts
 
 systemctl enable avahi-daemon.service
 systemctl start avahi-daemon.service
